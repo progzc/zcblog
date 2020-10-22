3f2f0ab9d185aa697aa1cc94ceec40430a7765b4
progzc <zcprog@foxmail.com>
2020-10-22 23:57:39 +0800

    '使用mavon搭建博客后台系统'

---
+++ zcblog-backend/zcblog-search/src/main/java/com/progzc/blog/BlogRunApplication.java
@@ -0,0 +1,19 @@
+package com.progzc.blog;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description 博客项目的启动类
+ * @Author zhaochao
+ * @Date 2020/10/22 23:26
+ * @Email zcprog@foxmail.com
+ * @Version V1.0
+ */
+@SpringBootApplication
+public class BlogRunApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BlogRunApplication.class, args);
+    }
+}
