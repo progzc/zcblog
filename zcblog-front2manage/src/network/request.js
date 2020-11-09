a6e5035ea539973ecae1b3d3f63ddd35441307c4
progzc <zcprog@foxmail.com>
2020-11-10 00:28:25 +0800

    '请求添加时间戳，完善axios的封装'

--- zcblog-front2manage/src/network/request.js
+++ zcblog-front2manage/src/network/request.js
@@ -6,7 +6,7 @@ import { clearLoginInfo } from 'common/js/utils/login'
 import merge from 'lodash/merge'
 
 export default function request (config) { // 封装网络请求
-  // 1.创建axios的示例
+  // 1. 创建axios的示例
   const instance = axios.create({ // 创建网络请求实例（若有不同配置，可以封装多个网络请求实例）
     baseURL: process.env.VUE_APP_API,
     timeout: 1000 * 10, // 最大延时10s
@@ -18,13 +18,15 @@ export default function request (config) { // 封装网络请求
 
   // 2.1 请求拦截
   instance.interceptors.request.use(config => {
-    if (config.params) { // get请求参数处理
-      config.params = merge(new Date().getTime(), config.params)
+    const timestamp = { // 对每次请求生成当前时间戳
+      t: new Date().getTime()
     }
-    if (config.data) { // post请求参数处理
-      config.data = JSON.stringify(merge(new Date().getTime(), config.data))
+    if (config.params) { // get请求参数处理添加时间戳，并json化
+      config.params = merge(timestamp, config.params)
     }
-
+    if (config.data) { // post请求参数添加时间戳，并json化
+      config.data = JSON.stringify(merge(timestamp, config.data))
+    }
     config.headers.token = Vue.cookie.get('token') // 请求头带上token
     return config
   }, error => {
@@ -44,6 +46,6 @@ export default function request (config) { // 封装网络请求
     return Promise.reject(error)
   })
 
-  // 3.发送真正的网络请求
+  // 3. 发送真正的网络请求
   return instance(config)
 }
