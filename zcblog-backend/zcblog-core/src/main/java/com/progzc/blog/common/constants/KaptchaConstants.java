874911f3248298f91ff093e701a29f4563ea3e58
progzc <zcprog@foxmail.com>
2020-11-14 23:00:05 +0800

    '使用多线程和RestTemplate模拟用户登录过程'

---
+++ zcblog-backend/zcblog-core/src/main/java/com/progzc/blog/common/constants/KaptchaConstants.java
@@ -0,0 +1,12 @@
+package com.progzc.blog.common.constants;
+
+/**
+ * @Description 记录Kaptcha每次生成的验证码：只自动登录时起作用
+ * @Author zhaochao
+ * @Date 2020/11/14 16:12
+ * @Email zcprog@foxmail.com
+ * @Version V1.0
+ */
+public class KaptchaConstants {
+    public static String captcha = "";
+}
