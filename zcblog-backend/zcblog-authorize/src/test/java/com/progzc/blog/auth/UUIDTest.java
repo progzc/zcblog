f4592ad8718ae100d8c8a77049b952218d3aba80
progzc <zcprog@foxmail.com>
2020-11-14 23:53:10 +0800

    '移动UUIDTest的位置'

---
+++ zcblog-backend/zcblog-authorize/src/test/java/com/progzc/blog/auth/UUIDTest.java
@@ -0,0 +1,20 @@
+package com.progzc.blog.auth;
+
+import org.junit.Test;
+
+import java.util.UUID;
+
+/**
+ * @Description 测试UUID
+ * @Author zhaochao
+ * @Date 2020/11/14 10:17
+ * @Email zcprog@foxmail.com
+ * @Version V1.0
+ */
+public class UUIDTest {
+
+    @Test
+    public void test() {
+        System.out.println(UUID.randomUUID().toString());
+    }
+}
