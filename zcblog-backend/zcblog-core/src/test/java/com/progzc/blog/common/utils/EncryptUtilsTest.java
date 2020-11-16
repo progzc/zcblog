18423224cec248dc2b2be5556a9be6347fa3f765
progzc <zcprog@foxmail.com>
2020-11-16 15:08:11 +0800

    '测试AES加密传输'

---
+++ zcblog-backend/zcblog-core/src/test/java/com/progzc/blog/common/utils/EncryptUtilsTest.java
@@ -0,0 +1,20 @@
+package com.progzc.blog.common.utils;
+
+import org.junit.Test;
+
+/**
+ * @Description 加密解密工具类测试
+ * @Author zhaochao
+ * @Date 2020/11/16 1:51
+ * @Email zcprog@foxmail.com
+ * @Version V1.0
+ */
+public class EncryptUtilsTest {
+    @Test
+    public void test(){
+        String plaintext = "admin123";
+        System.out.println("明文：" + plaintext);
+        System.out.println("加密：" + EncryptUtils.encrypt(plaintext));
+        System.out.println("解密：" + EncryptUtils.decrypt(EncryptUtils.encrypt(plaintext)));
+    }
+}
